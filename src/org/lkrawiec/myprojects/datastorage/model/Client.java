5dc1e40ce3f7a68b2650eaef71e67c9db8c9769d
¨Lukasz <lukaszk1994@live.nl>
2018-06-18 20:54:55 +0200

    init commit

---
+++ src/org/lkrawiec/myprojects/datastorage/model/Client.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lkrawiec.myprojects.datastorage.model;
+
+/**
+ *
+ * @author lukii
+ */
+public class Client {
+    
+    private String name;
+    private String phoneNumber;
+    private String address;
+    private String email;
+    
+}
